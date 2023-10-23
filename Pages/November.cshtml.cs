635066d8b694eef4a5af7d4c95fc7e2122de621a
Nigel <m0cvo@yahoo.co.uk>
2023-10-23 08:06:03 +0100

    updated data and prepared new chart for November 2023

---
+++ Pages/November.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherComparison.Pages
+{
+    public class NovemberModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
