72373b798cc180636bf072fe8761f7fce8d3a2e4
Nigel <m0cvo@yahoo.co.uk>
2023-11-07 07:50:07 +0000

    Added a page and chart for December 2023 in readiness.

---
+++ Pages/December.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherComparison.Pages
+{
+    public class DecemberModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
