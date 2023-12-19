73c454664b347337f72cdad978e8e4fb58611a5e
Nigel <m0cvo@yahoo.co.uk>
2023-12-19 07:31:16 +0000

    Added Jan24 page.

---
+++ Pages/Jan24.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherComparison.Pages
+{
+    public class Jan24Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
