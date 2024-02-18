bb8db5056d44f4b5f2b08eda18413d3afcb9e23e
Nigel <m0cvo@yahoo.co.uk>
2024-02-18 07:58:45 +0000

    Added March

---
+++ Pages/March24.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherComparison.Pages
+{
+    public class March24Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
