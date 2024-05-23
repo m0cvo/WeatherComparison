aab23f114bad712740701bb37951b983ffb91d61
Nigel <m0cvo@yahoo.co.uk>
2024-05-23 12:33:01 +0100

    Added June

---
+++ Pages/June24.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherComparison.Pages
+{
+    public class June24Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
