9840eb716985b4f75e259aa9fdb590eebde4d43f
Nigel <m0cvo@yahoo.co.uk>
2024-09-27 07:47:16 +0100

    New page for October.

---
+++ Pages/October24.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherComparison.Pages
+{
+    public class October24Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
