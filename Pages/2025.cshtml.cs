abbec03aca2bae07cf069820b112479485087f33
Nigel <m0cvo@yahoo.co.uk>
2024-11-02 09:51:29 +0000

    Started creating new menu system.

---
+++ Pages/2025.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherComparison.Pages
+{
+    public class _2025Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
