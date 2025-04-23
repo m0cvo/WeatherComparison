d42fb09b8c8dfdcdc0624b91b93645833bd9e74a
Nigel <m0cvo@yahoo.co.uk>
2025-04-23 08:20:51 +0100

    Data
    Added page for May 2025

---
+++ Pages/2025/May25.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherComparison.Pages._2025
+{
+    public class May25Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
