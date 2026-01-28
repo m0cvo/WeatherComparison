ae9a61831ba486ba903f748d70ca7070e9172d71
Nigel <m0cvo@yahoo.co.uk>
2026-01-28 07:46:34 +0000

    New page for February

---
+++ Pages/2026/Feb26.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherComparison.Pages._2026
+{
+    public class Feb26Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
