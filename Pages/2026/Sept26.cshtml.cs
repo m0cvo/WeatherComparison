da2ee57ccfb3f5761386905c097c8ada939bffc8
Nigel <m0cvo@yahoo.co.uk>
2026-08-26 07:55:32 +0100

    New page - September

---
+++ Pages/2026/Sept26.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherComparison.Pages._2026
+{
+    public class Sept26Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
